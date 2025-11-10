da49f1dcbc4e89229f2e9207c82a41090a666fe9
Shockingnews <gagemckenzie70@gmail.com>
2025-11-10 12:03:36 -0400

    fixed glitch

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -3,15 +3,38 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     static float Xmovement;
+
+    static Vector3 startPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Xmovement += 1;
+            Vector3 test = new Vector3(Xmovement, 0f, 0f);
+
+            transform.position = test;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Xmovement += 1;
+            Vector3 test = new Vector3(Xmovement, 0f, 0f);
+
+            transform.position = test;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Xmovement += 1;
+            Vector3 test = new Vector3(Xmovement, 0f, 0f);
+
+            transform.position = test;
+        }
         if (Input.GetKeyDown(KeyCode.W))
         {
             Xmovement += 1;
