120fa7ab2065756bfc1632a4ecad9c91a0e90e7e
Gage Mckenzie <gagemckenzie70@gmail.com>
2025-11-13 11:59:34 -0400

    starting form scratch

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -2,54 +2,24 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class playerMovement : MonoBehaviour
 {
-    static float Xmovement;
-    static float Zmovement;
+    
 
     static float acceleration;
     static float deceleration;
 
     private CharacterController controller;
 
-    static Vector3 startPos;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = startPos;
-        Xmovement = transform.position.x + 1;
-        Zmovement = transform.position.z + 1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetKey(KeyCode.W))
-        {
-            
-            Vector3 test = new Vector3(Xmovement, 0f, Zmovement);
-
-            transform.position = Vector3.MoveTowards(transform.position, test,1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Xmovement -= 1;
-            Vector3 test = new Vector3(Xmovement, 0f, Zmovement);
-
-            transform.position = test;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Zmovement -= 1;
-            Vector3 test = new Vector3(Xmovement, 0f, Zmovement);
-
-            transform.position = test;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Zmovement += 1;
-            Vector3 test = new Vector3(Xmovement, 0f, Zmovement);
-
-            transform.position = test;
-        }
+        
     }
 }
