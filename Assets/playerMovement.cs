d00685bd2fb9bc6409ea3e68832bc685b5829451
Gage Mckenzie <gagemckenzie70@gmail.com>
2025-11-17 19:21:02 -0400

    movement works now

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -6,45 +6,31 @@ public class playerMovement : MonoBehaviour
 
     static float acceleration;
     static float deceleration;
-    static int playerspeed = 5;
+    public int playerspeed = 5;
 
-    public GameObject player;
+    
 
     private CharacterController controller;
 
-    public InputActionReference movement;
+    
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        //Vector3.MoveTowards();
-        controller = gameObject.AddComponent<CharacterController>();
+        
+        controller = GetComponent<CharacterController>();
     }
     
-    private void OnEnable()
-    {
-        movement.action.Enable();
-    }
-    private void OnDisable()
-    {
-        movement.action.Disable();
-    }
+   
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementInput = movement.action.ReadValue<Vector2>();
-        Vector3 movePlayer = new Vector3(movementInput.x, 0, movementInput.y);
+        
+        Vector3 movePlayer = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        movePlayer = Vector3.ClampMagnitude(movePlayer, 1f);
-
-        if(movePlayer != Vector3.zero)
-        {
-            transform.forward = movePlayer;
-        }
-
-        Vector3 newmovement = (playerspeed * movePlayer);
-         controller.Move(newmovement * Time.deltaTime);
+        
+         controller.Move(movePlayer * Time.deltaTime * playerspeed);
     }
 }
