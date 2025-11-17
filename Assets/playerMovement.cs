400fdff94be093729b9655edf28ffd82c8512c6c
Shockingnews <gagemckenzie70@gmail.com>
2025-11-17 11:48:45 -0400

    test movement code

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -9,17 +9,40 @@ public class playerMovement : MonoBehaviour
 
     private CharacterController controller;
 
+    public InputActionReference movement;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        //Vector3.MoveTowards();
+        
     }
-
+    private void Awake()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+    private void OnEnable()
+    {
+        movement.action.Enable();
+    }
+    private void OnDisable()
+    {
+        movement.action.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
-        
-        
+        Vector2 movementInput = movement.action.ReadValue<Vector2>();
+        Vector3 movePlayer = new Vector3(movementInput.x, 0f, movementInput.y);
+
+        movePlayer = Vector3.ClampMagnitude(movePlayer, 1f);
+
+        if(movePlayer != Vector3.zero)
+        {
+            transform.forward = movePlayer;
+        }
+        controller.Move(movePlayer * Time.deltaTime);
     }
 }
