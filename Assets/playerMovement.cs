f78ddd6a03e4f718dd687baf192646fc96d4fecd
Shockingnews <gagemckenzie70@gmail.com>
2025-11-18 11:22:26 -0400

    reverted back to old movement
    It made more cents

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -2,35 +2,42 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class playerMovement : MonoBehaviour
 {
-    
+
 
     static float acceleration;
     static float deceleration;
-    public int playerspeed = 5;
+    public float playerspeed = 5;
 
-    
+
 
     private CharacterController controller;
 
-    
+    public InputActionReference movement;
 
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        
-        controller = GetComponent<CharacterController>();
+        // Gets the controller adds it to the game object
+        controller = gameObject.GetComponent<CharacterController>();
     }
-    
-   
+
+
     // Update is called once per frame
     void Update()
     {
-        
-        Vector3 movePlayer = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // gets the input from the player and reads it out
+        Vector2 movementInput = movement.action.ReadValue<Vector2>();
+        // gets the x and y values of the player input and puts in a 3d space
+        Vector3 movePlayer = new Vector3(movementInput.x, 0, movementInput.y);
 
-        
-         controller.Move(movePlayer * Time.deltaTime * playerspeed);
+
+        // moves where player is facing
+        transform.forward = movePlayer;
+
+        // makes movement faster
+        Vector3 newmovement = (playerspeed * movePlayer);
+        controller.Move(newmovement * Time.deltaTime);
     }
 }
